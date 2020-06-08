9fb26c5d393e14ced3b9895981b13b1902cd5214
Bruno Batalha <64993013+brbatalha@users.noreply.github.com>
2020-06-07 23:32:53 -0300

    Create brackets_checker.py

---
+++ Projetos/brackets_checker.py
@@ -0,0 +1,22 @@
+open_list = ["[", "{", "("]
+close_list = ["]", "}", ")"]
+
+
+def check(txt):
+    stack = []
+    for i in txt:
+        if i in open_list:
+            stack.append(i)
+        elif i in close_list:
+            pos = close_list.index(i)
+            if len(stack) > 0 and open_list[pos] == stack[len(stack)-1]:
+                stack.pop()
+            else:
+                return "Unbalanced"
+    if len(stack) == 0:
+        return "Balanced"
+    return "Unbalanced"
+
+
+n = input()
+print(check(n))
